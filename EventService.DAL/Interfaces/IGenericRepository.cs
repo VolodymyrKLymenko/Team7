451a4b9e3a07bdd55db4b48c04faa0e72682d9ee
Markevych Oleh <omarkevych@leobit.com>
2020-05-21 13:33:56 +0300

    Added Generic Repository

---
+++ EventService.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EventService.DAL.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity item);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        void Update(TEntity item);
+        IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties);
+        IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties);
+    }
+}
