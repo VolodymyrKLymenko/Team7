c3d4086f29eb8606a2c2c4c79cfc23f904d0fe73
Markevych Oleh <omarkevych@leobit.com>
2020-05-21 13:19:37 +0300

    Added entities classes

---
+++ EventService.DAL/Entities/EventLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventService.DAL.Entities
+{
+    public class EventLog
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+        public int EventActionId { get; set; }
+        public EventAction EventAction { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ EventService.DAL/Entities/Faculty.cs
@@ -0,0 +1,11 @@
+﻿namespace EventService.DAL.Entities
+{
+    public class Faculty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int UniversityId { get; set; }
+        public University University { get; set; }
+    }
+}
