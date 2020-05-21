4e6282478bc2e6fd6ee3438b436be429e2ea7cf3
Markevych Oleh <omarkevych@leobit.com>
2020-05-21 13:27:01 +0300

    Added Context of DB and installed EF Core

---
+++ EventService.DAL/Context/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using EventService.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventService.DAL.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventAction> EventActions { get; set; }
+        public DbSet<EventLog> EventLogs { get; set; }
+        public DbSet<EventStatus> EventStatuses { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<University> Universities { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
